fba7f58a83a5c1f598f8ddd36ec5499d69f3d03c
Kyle Gallagher <litonfiredesign@gmail.com>
2019-03-09 13:02:51 -0500

    init commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import '../sass/main.scss';
+require("../index.html");
+
+
+//////////// POLYFILLS ///////////////////////
+// modern svg sprite file usage in all browsers ( ie11 support )
+import svgxuse from 'svgxuse';
+// Bubble up event delegations
+// import elementClosest from 'element-closest';
---
+++ webpack.config.js
@@ -0,0 +1,124 @@
+/* === dont forget to import scss to main.js file === */
+/* ===> import './main.scss'; <=== */
+
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
+module.exports = {
+    // if we need multiple entry points they are passed in via an array
+    entry: './src/js/index.js',
+    output: {
+        // uses nodejs path built in function to handle the absolute path to the root folder
+        path: path.resolve(__dirname, 'dist'),
+        // name of the final output file
+        filename: 'scripts/min.js'
+    },
+    optimization: {
+        minimizer: [
+            // minifyjs
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false, // set to true if you want JS source maps
+                extractComments: true
+            }),
+            // minifycss
+            new OptimizeCssAssetsPlugin({})
+        ],
+        // this is used to export the css from the js file import into a seperate css file
+        splitChunks: {
+          cacheGroups: {
+            styles: {
+              name: 'styles',
+              test: /\.css$/,
+              chunks: 'all',
+              enforce: true
+            }
+          }
+        },
+      },
+    devServer: {
+        contentBase: './src',
+        watchContentBase: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "scripts/min.css",
+            // chunkFilename: "[id].css"
+        })
+    ],
+    module: {
+        rules:[
+            {   
+                test: /\.js$/,
+                exclude: /node_modules/, 
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                      presets: [
+                        ["@babel/preset-env", {
+                            useBuiltIns: 'usage'
+                        }], // uses preset-env and calls on babel polyfils based on usage
+                      ],
+                    babelrc: false
+                    }
+                }]
+             },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader, // extracts css from js and makes a sep cssfile
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    {
+                        loader: "css-loader", // translates CSS into CommonJS
+                    },
+                    {
+                        loader: 'postcss-loader', // used to autoprefix
+                        options: {plugins:[require('autoprefixer')()]}
+                    },
+                    {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    { loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]"
+                    }
+                    },
+                    { loader: "extract-loader" },
+                    { loader: "html-loader",
+                    options: {
+                        attrs: ["img:src", "use:href"],
+                        minimize: true,
+                    }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use: [
+                  { loader: "file-loader",
+                    options: {
+                      name: "images/[sha512:hash:hex:5]-[name].[ext]", // hash images so if changed, people see newest versions
+                    }
+                  }
+                ]
+            },
+        ]
+    },
+}
+
+// One big note:
+// outputPath is place where your want to save files
+// publicPath is what url you have in js, css and etc files.
