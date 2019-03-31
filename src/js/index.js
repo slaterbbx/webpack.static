28d1a559f97a76e792d6136852ae54a33a51dc83
Kyle Gallagher <litonfiredesign@gmail.com>
2019-03-31 19:05:17 -0400

    removed useless comments

--- src/js/index.js
+++ src/js/index.js
@@ -5,5 +5,3 @@ require("../index.html");
 //////////// POLYFILLS ///////////////////////
 // modern svg sprite file usage in all browsers ( ie11 support )
 import svgxuse from 'svgxuse';
-// Bubble up event delegations
-// import elementClosest from 'element-closest';
