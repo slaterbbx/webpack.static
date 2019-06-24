6d964586c76374ca8f547d385d606e251f5f5fda
Kyle Gallagher <litonfiredesign@gmail.com>
2019-06-24 00:32:45 -0400

    added comment

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,3 @@
+// Import files for Webpack entrypoint
 import '../sass/main.scss';
 require("../index.html");
-
-
-//////////// POLYFILLS ///////////////////////
-// modern svg sprite file usage in all browsers ( ie11 support )
-import svgxuse from 'svgxuse';
